021996a589d2b33d2c8a41471b740c54cbde6fd2
yogramming <yogeshdubey9336@gmail.com>
2026-04-01 17:44:06 +0530

    Initiate spring boot project

---
+++ src/main/java/com/yogramming/devpulse/DevpulseApplication.java
@@ -0,0 +1,13 @@
+package com.yogramming.devpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevpulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yogramming/devpulse/DevpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogramming.devpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
